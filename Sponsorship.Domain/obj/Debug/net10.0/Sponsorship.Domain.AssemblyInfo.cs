30331819a7ec6ab3c20f4f7a2344386c715dc6e8
Rashedul Alam <rashed464@outlook.com>
2026-05-21 22:38:57 +0600

    File updated

--- Sponsorship.Domain/obj/Debug/net10.0/Sponsorship.Domain.AssemblyInfo.cs
+++ Sponsorship.Domain/obj/Debug/net10.0/Sponsorship.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sponsorship.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67af0b0238bf5458465b6fa607d15757f56863e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55e55161f255e78e3aa952c829130afeeb192843")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sponsorship.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sponsorship.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
