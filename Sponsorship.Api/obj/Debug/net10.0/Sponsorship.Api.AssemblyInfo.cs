f0df3f1c4c175b042cb97f6ff9a9faba6a209926
Rashedul Alam <rashed464@outlook.com>
2026-05-26 00:59:49 +0600

    Docker file updated withc cros

--- Sponsorship.Api/obj/Debug/net10.0/Sponsorship.Api.AssemblyInfo.cs
+++ Sponsorship.Api/obj/Debug/net10.0/Sponsorship.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sponsorship.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e44aaed4a7d5e9a03efe545373fe5cfeee366bec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0239648a4ab5f3ce15628db25b4f5ea6abae4ae4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sponsorship.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sponsorship.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
